fbc4801be4f420b033fecc0bdeae5eb1538ca6b7
kennyhelsens <kennyhelsens@7ab3b2d0-b2f1-d46a-9579-b15549e08516>
2011-05-26 08:07:37 +0000

    - updated documentation for the CoreConfiguration.

--- jtraml/src/main/java/com/compomics/traml/config/CoreConfiguration.java
+++ jtraml/src/main/java/com/compomics/traml/config/CoreConfiguration.java
@@ -3,25 +3,33 @@ package com.compomics.traml.config;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
-import java.io.File;
-
 /**
  * This class is a Configuration hub for the Sep To Traml tool.
  */
 public class CoreConfiguration {
 
+    /**
+     * The current version of the library.
+     */
     public final static String VERSION = "0.1";
 
+    /**
+     * Properties instance to manage the properties of the library.
+     */
     private static PropertiesConfiguration input;
-    private static File iWorkSpace = null;
-
-// -------------------------- STATIC METHODS --------------------------
 
 
+    /**
+     * The standard separation character to be used by the library.
+     * @return
+     */
     public static String getSeparationChar() {
         return input.getString("sep.char");
     }
 
+    /**
+     * Static initiatlizer.
+     */
     static {
         try {
             input = new PropertiesConfiguration("config/input.properties");
