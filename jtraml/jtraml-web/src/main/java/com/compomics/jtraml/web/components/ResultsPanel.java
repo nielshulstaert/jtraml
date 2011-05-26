57470b8ece2c7e5e35637f68502123df58dfc292
kennyhelsens <kennyhelsens@7ab3b2d0-b2f1-d46a-9579-b15549e08516>
2011-05-26 16:15:00 +0000

    - refactoring of components in the jtraml-web module

--- jtraml/jtraml-web/src/main/java/com/compomics/jtraml/web/components/ResultsPanel.java
+++ jtraml/jtraml-web/src/main/java/com/compomics/jtraml/web/components/ResultsPanel.java
@@ -1,4 +1,4 @@
-package com.compomics.jtraml.web.panel;
+package com.compomics.jtraml.web.components;
 
 import com.compomics.jtraml.model.ConversionJobOptions;
 import com.compomics.jtraml.web.TramlConverterApplication;
