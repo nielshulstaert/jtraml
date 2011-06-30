13a31820270a6bf943db2f25886709425f9663ba
kennyhelsens <kennyhelsens@7ab3b2d0-b2f1-d46a-9579-b15549e08516>
2011-06-30 09:34:53 +0000

    - updated to version 0.4

--- jtraml/jtraml-core/src/main/java/com/compomics/jtraml/config/CoreConfiguration.java
+++ jtraml/jtraml-core/src/main/java/com/compomics/jtraml/config/CoreConfiguration.java
@@ -11,7 +11,7 @@ public class CoreConfiguration {
     /**
      * The current version of the library.
      */
-    public final static String VERSION = "0.3";
+    public final static String VERSION = "0.4";
 
     /**
      * The current version of the TraML XSD.
