6dd133c10395e00f8faed8e4816468e79489900a
kennyhelsens <kennyhelsens@7ab3b2d0-b2f1-d46a-9579-b15549e08516>
2011-10-19 11:40:25 +0000

    - made the Analytics configurable via properties - updated to version 1.0.2

--- trunk/jtraml/jtraml-web/src/main/java/com/compomics/jtraml/web/analytics/AnalyticsLogger.java
+++ trunk/jtraml/jtraml-web/src/main/java/com/compomics/jtraml/web/analytics/AnalyticsLogger.java
@@ -1,6 +1,7 @@
 package com.compomics.jtraml.web.analytics;
 
 import com.compomics.jtraml.model.ConversionJobOptions;
+import com.compomics.jtraml.web.config.WebConfiguration;
 import org.apache.log4j.Logger;
 
 import java.util.Map;
@@ -13,29 +14,36 @@ public class AnalyticsLogger {
     private static Logger logger = Logger.getLogger(AnalyticsLogger.class);
     private static final char sep = ';';
 
+    private static boolean doAnalytics = WebConfiguration.doAnalytics();
     /**
      * Log a statement when a new session is created
      * @param aSessionID
      */
     public static void newSession(String aSessionID){
-        analytics.info(aSessionID + sep + "NEW_SESSION");
+        logMetric(aSessionID + sep + "NEW_SESSION");
     }
 
 
     public static void startConversion(String aSessionID) {
-        analytics.info( aSessionID + sep + "NEW_CONVERSION");
+        logMetric( aSessionID + sep + "NEW_CONVERSION");
     }
 
     public static void endConversion(String aSessionID, ConversionJobOptions aOptions) {
-        analytics.info( aSessionID + sep + "END_CONVERSION");
-        analytics.info( aSessionID + sep + "JOB_IMPORTTYPE" + sep + aOptions.getImportType().getName());
-        analytics.info( aSessionID + sep + "JOB_EXPORTTYPE" + sep + aOptions.getExportType().getName());
+        logMetric( aSessionID + sep + "END_CONVERSION");
+        logMetric( aSessionID + sep + "JOB_IMPORTTYPE" + sep + aOptions.getImportType().getName());
+        logMetric( aSessionID + sep + "JOB_EXPORTTYPE" + sep + aOptions.getExportType().getName());
     }
 
     public static void startConversionViaURL(String aSessionID, Map aParams) {
-        analytics.info( aSessionID + sep + "NEW_URL_JOB");
+        logMetric( aSessionID + sep + "NEW_URL_JOB");
         for (Object lParam: aParams.keySet()) {
-            analytics.info( aSessionID + sep + "NEW_URL_JOB_" + lParam.toString().toUpperCase() + sep + aParams.get(lParam));
+            logMetric( aSessionID + sep + "NEW_URL_JOB_" + lParam.toString().toUpperCase() + sep + aParams.get(lParam));
+        }
+    }
+
+    private static void logMetric(String aMessage){
+        if(doAnalytics){
+            analytics.info(aMessage);
         }
     }
 }
